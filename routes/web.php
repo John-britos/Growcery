f1b3e9c241e6a087fd3bc691a40b49b54a4d5809
John-dev <johnbritosash@gmail.com>
2025-03-28 19:07:17 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\Admin\AdminMainController;
+use App\Http\Middleware\RoleManager;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,7 +10,26 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'rolemanager:customer' ])->name('dashboard');
+
+//admin routes
+Route::middleware(['auth', 'verified', 'rolemanager:admin' ])->group(function () {
+    Route::controller(AdminMainController::class)->group(function () {
+        Route::prefix('admin')->group(function () {
+           Route::get('/dashboard', 'index') -> name('admin');
+           });
+        });
+    });
+
+
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.admin');
+})->middleware(['auth', 'verified', 'rolemanager:admin' ])->name('admin');
+
+Route::get('/seller/dashboard', function () {
+    return view('seller');
+})->middleware(['auth', 'verified', 'rolemanager:seller' ])->name('seller');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
